00ca9930dc42eaa55e2407e9f0f9188cc0e93614
Aymansaeed9542 <aym9542@gmail.com>
2025-09-02 00:06:29 +0300

    handling loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <i className="fas fa-spinner fa-spin text-7xl"></i>
+    </div>
+  )
+}
