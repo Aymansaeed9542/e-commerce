6c0ffe5a5a5c76aee9800f9a277f0ee00994d07a
Aymansaeed9542 <aym9542@gmail.com>
2025-09-17 16:46:54 +0300

    solve server problem

--- src/apis/allProducts.ts
+++ src/apis/allProducts.ts
@@ -1,7 +1,10 @@
 
     export default async function getAllProducts(){
 
-            const response = await fetch(`${process.env.NEXT_URL}/api/users`)
+            const response = await fetch(`${process.env.NEXT_URL}/api/users`,{
+    cache: "no-store"}
+)
+
     const {data}= await response.json()
         return data
     }
