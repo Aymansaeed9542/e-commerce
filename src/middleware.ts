0a57452fafcacc317e528ac7cbce2890d41dea82
Aymansaeed9542 <aym9542@gmail.com>
2025-09-17 21:35:11 +0300

    feat(auth): add middleware for route protection & redirects
    - Implemented NextAuth JWT middleware to protect private routes
    - Redirect unauthenticated users from protected routes to /login
    - Redirect authenticated users away from /login and /register to home
    - Fixed matcher config by using absolute paths

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export async function middleware(request: NextRequest) {
+const {pathname} = request.nextUrl
+const routes = ["/", "/brands" , "/categories" , "/cart" , "/productDetails"]
+const authPages = ["/login" ,"/register"]
+
+const token = await getToken({req : request})
+if(!token && routes.includes(pathname) ){
+  return NextResponse.redirect(new URL('/login' , request.url))
+
+}
+if(token && authPages.includes(pathname) ){
+  return NextResponse.redirect(new URL('/' , request.url))
+
+}
+
+  return NextResponse.next()
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/brands" , "/categories" , "/cart" , "/productDetails","/login" ,"/register"],
+}
