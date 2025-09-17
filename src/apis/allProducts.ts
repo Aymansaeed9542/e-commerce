da11ec48ded0f3f13988847175f1fab0d39b9f7d
Aymansaeed9542 <aym9542@gmail.com>
2025-09-17 16:42:07 +0300

    solve deployment error

--- src/apis/allProducts.ts
+++ src/apis/allProducts.ts
@@ -1,7 +1,7 @@
 
     export default async function getAllProducts(){
 
-            const response = await fetch("http://localhost:3000/api/users")
+            const response = await fetch(`${process.env.NEXT_URL}/api/users`)
     const {data}= await response.json()
         return data
     }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest , NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-    const res = await fetch(`${process.env.API}/products`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
     const {data}= await res.json()
 
 
